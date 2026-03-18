cf7a6f058dc419f4d2810a7dedc1b0aa818eb200
Hirlei Carlos <prof.hirleicarlos@gmail.com>
2026-03-18 18:14:54 -0300

    servidor
    Container para node.js

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-set -e
-
-PROJECT_ROOT="/var/www/html"
-
-if [ -d "$PROJECT_ROOT" ]; then
-
-    echo "Corrigindo permissões dos projetos em $PROJECT_ROOT..."
-
-    chown -R www-data:www-data "$PROJECT_ROOT"
-
-    find "$PROJECT_ROOT" -type d -exec chmod 755 {} \;
-    find "$PROJECT_ROOT" -type f -exec chmod 644 {} \;
-    
-fi
+set -eu
 
 exec "$@"
